eda01e15d21e665bc2e9a4deb12bd9e9eaa89f31
ranji <letsmailjith@gmail.com>
2025-06-08 11:12:42 +0530

    Code Transferred

---
+++ demoproject/src/test/java/testScript/Base.java
@@ -0,0 +1,28 @@
+package testScript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void browserInitialisation()
+    {
+        driver=new ChromeDriver();
+    	driver.get("https://groceryapp.uniqassosiates.com/admin");
+    	driver.manage().window().maximize();
+    	
+    }
+	
+	
+	//@AfterMethod
+	public void driverQuit() {
+		//driver.close();
+    	driver.quit();
+	}	
+	
+}
