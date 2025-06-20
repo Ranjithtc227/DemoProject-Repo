4fd884ddd34b51969247a6fee89c5afadd5265aa
ranji <letsmailjith@gmail.com>
2025-06-20 20:25:05 +0530

    Code Transferred

--- demoproject/src/test/java/testScript/AdminUsersTest.java
+++ demoproject/src/test/java/testScript/AdminUsersTest.java
@@ -19,15 +19,15 @@ public class AdminUsersTest extends Base{
 		        //String username="Ranjith";
 				//String password="Ranjith@321"; 
 				
-		       // String username=ExcelUtility.getStringData(1, 0, "loginpage");
-		       // String password=ExcelUtility.getStringData(1, 1, "loginpage");
+		        String username=ExcelUtility.getStringData(1, 0, "loginpage");
+		        String password=ExcelUtility.getStringData(1, 1, "loginpage");
 		        
 		        //String user=ExcelUtility.getStringData(1, 0, "adminusers");
 				//String pass=ExcelUtility.getStringData(1, 1, "adminusers");
 				
 				LoginPage loginpage=new LoginPage(driver);
-				//loginpage.enterTheUserName(username);
-				//loginpage.enterThePassword(password);
+				loginpage.enterTheUserName(username);
+				loginpage.enterThePassword(password);
 				loginpage.clickTheSignInButton();
 				
 				AdminUsersPage adminuserspage=new AdminUsersPage(driver);
